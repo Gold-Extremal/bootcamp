ba817385bdefae1f20a3a474490955314b518651
Gold-Extremal <a.v.mandrin@gmail.com>
2023-02-05 19:53:39 +0600

    Это проще переписать с нуля...

--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
-import classes from './Title.module.css'  
+import { useTimer } from 'use-timer';
+import classes from './Clock.module.css'
+import arrow from './arrow.svg'
 
 function Clock() {
+  
+  const { time, start, pause, reset, status } = useTimer({
+    initialTime: 902,
+    endTime: 0,
+    timerType: 'DECREMENTAL',
+  });
+
+  const humanReadable = (time) => {
+    let n = ("0"+ Math.floor(time / 3600)).slice(-2) + ":" + ("0"+ Math.floor((time % 3600)/60)).slice(-2) + ":" + ("0"+ Math.floor((time % 3600)%60)).slice(-2);
+    return n ;
+  }
+
   return (
-    <div className={classes.Clock}>
-      
+    <div>
+      <div className={classes.clock}>
+        <div className={classes.border}></div>
+        <p className={classes.timeValue}>{humanReadable(time)}</p>
+      </div>
+
+      <div className={classes.menu}>
+      <button className={classes.btn} onClick={pause}>Pause</button>
+        <button className={classes.start} onClick={start}><img src={arrow}></img></button>
+        <button className={classes.btn} onClick={reset}>Reset</button>
+      </div>
     </div>
+    
   );
 }
 
--- src/components/Management/Management.jsx
+++ src/components/Management/Management.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import './Management.module.css' 
+import { useTimer } from 'use-timer';
+import classes from './Management.module.css' 
 
 function Management() {
+
+  // const { time, start, pause, reset, status } = useTimer({
+  //   initialTime: 902,
+  //   endTime: 0,
+  //   timerType: 'DECREMENTAL',
+  // });
+
+  // const humanReadable = (time) => {
+  //   let n = ("0"+ Math.floor(time / 3600)).slice(-2) + ":" + ("0"+ Math.floor((time % 3600)/60)).slice(-2) + ":" + ("0"+ Math.floor((time % 3600)%60)).slice(-2);
+  //   return n ;
+  // }
+
   return (
     <div className="Management">
-      
+      {/* <button onClick={start}>Start</button>
+      <button onClick={pause}>Pause</button>
+      <button onClick={reset}>Reset</button> */}
     </div>
   );
 }
